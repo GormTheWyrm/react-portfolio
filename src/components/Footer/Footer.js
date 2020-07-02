f8794f2a41d3ef13d8ad21a09a8b9214148e9a68
GormTheWyrm <gdsoucie@gmail.com>
2020-07-01 22:11:04 -0400

    transfer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Footer(){
+
+return (
+
+<div>
+
+<span>This Application was created using React</span>
+<p>The source material can be seen at this repo https://github.com/GormTheWyrm/react-portfolio</p>
+
+</div>
+
+);
+
+}
+export default Footer;
