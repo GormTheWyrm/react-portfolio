862e7734e5877eec07668e920f7cff05565484e5
Gorm <gdsoucie@gmail.com>
2020-07-23 18:27:40 -0400

    Updates

---
+++ precache-manifest.0d0b172336b5704d67f2e44ce510615b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7710dec02673777db84b3ea3439eacf9",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "4e8f5b91f1d75fa22d82",
+    "url": "/react-portfolio/static/css/main.ced29b66.chunk.css"
+  },
+  {
+    "revision": "68557f95ff94a573efd6",
+    "url": "/react-portfolio/static/js/2.85205b03.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio/static/js/2.85205b03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e8f5b91f1d75fa22d82",
+    "url": "/react-portfolio/static/js/main.95168534.chunk.js"
+  },
+  {
+    "revision": "0869a31269904f2fa32e",
+    "url": "/react-portfolio/static/js/runtime-main.b4ecae80.js"
+  },
+  {
+    "revision": "8344c609e390ad182964c4768cb62b73",
+    "url": "/react-portfolio/static/media/AboutTime.8344c609.PNG"
+  },
+  {
+    "revision": "170ef7b5347c47e376606917117f314b",
+    "url": "/react-portfolio/static/media/Dayplanner.170ef7b5.PNG"
+  },
+  {
+    "revision": "9dbfb5264027ac933bef0099bbb578c7",
+    "url": "/react-portfolio/static/media/PasswordGen.9dbfb526.PNG"
+  },
+  {
+    "revision": "8cf697f4abb12c457eaef9cb46f89330",
+    "url": "/react-portfolio/static/media/RVAfish.8cf697f4.PNG"
+  },
+  {
+    "revision": "8cd36d50fe3df1d6036489372691e637",
+    "url": "/react-portfolio/static/media/Rock.8cd36d50.PNG"
+  },
+  {
+    "revision": "1b0b37f6b6a8a74706725f57d1f091d5",
+    "url": "/react-portfolio/static/media/Weather.1b0b37f6.PNG"
+  }
+]);
